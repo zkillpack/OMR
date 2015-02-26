1d7da6544ed801d55977e9e90534b9cbf0078366
Dakota Killpack <killpack@princeton.edu>
2015-02-26 00:13:10 -0500

    add example scores

--- omr.py
+++ omr.py
@@ -3,15 +3,13 @@ import cv2
 import numpy as np
 from score import *
 
-DEBUG = False
-if DEBUG:
-    np.set_printoptions(threshold=np.nan)
 
-ap = argparse.ArgumentParser()
-ap.add_argument("-i", "--image", required=True, help="path to score image")
-args = vars(ap.parse_args())
-image = cv2.imread(args["image"])
+if __name__ == '__main__':
+	ap = argparse.ArgumentParser()
+	ap.add_argument("-i", "--image", required=True, help="path to score image")
+	args = vars(ap.parse_args())
+	image = cv2.imread(args["image"])
 
-score = Score(image)
+	score = Score(image)
 
-cv2.waitKey(0)
+	cv2.waitKey(0)
